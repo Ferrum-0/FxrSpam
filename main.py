d79fe9af0eaf7a0366ce514a63ad7e3c5c7a5954
Ferrum <kingferrum20@gmail.com>
2022-04-13 23:28:59 +0100

    Added IP Tracking

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ipaddress import ip_address
 import pyautogui as pg
 import time as t
 import socket as s
@@ -9,6 +10,7 @@ import os
 import json
 from urllib.request import urlopen
 from json import load
+import requests
 
 ctypes.windll.kernel32.SetConsoleTitleW("FxrSys - v2.3")
 
@@ -89,18 +91,12 @@ def main():
         print(result)
 
     def ipTracker():
-        addr = ' '
-        if addr == '':
-            url = 'https://ipinfo.io/json'
-        else:
-            url = 'https://ipinfo.io/' + addr + '/json'
-        res = urlopen(url)
-        #response from url(if res==None then check connection)
-        data = load(res)
-        #will load the json response into data
-        for attr in data.keys():
-            #will print the data line by line
-            print(attr,' '*13+'\t->\t',data[attr])
+        YOUR_GEOLOCATION_KEY = 'ec0cb00474be4d55a6bbf6a751609b40'
+        ip_address = input('Enter the IP Address you want to track: ')
+        response = requests.get('https://ipgeolocation.abstractapi.com/v1/?api_key=' + YOUR_GEOLOCATION_KEY + '&ip_address=' + ip_address)
+        result = json.dumps(json.loads(response.content), indent=1)
+        print(result)
+        
 
     if option == int(1):
         Spammer()
@@ -114,10 +110,14 @@ def main():
         asciiArt()
         t.sleep(5)
         main()
-    # if option == int(4):
-    #     ipTracker()
-    #     t.sleep(5)
-    #     main()
+    if option == int(4):
+        ipTracker()
+        t.sleep(5)
+        main()
+    if option == int(5):
+        print('Please provide a valid number.')
+        t.sleep(2)
+        main()
 
 
 main()
