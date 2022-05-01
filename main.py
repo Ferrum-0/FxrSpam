0c5a9f58d529c832974b0a7dfe3c16b802aeacf9
Ferrum <kingferrum20@gmail.com>
2022-05-01 13:47:51 +0100

    YRS

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ipaddress import ip_address
 from itertools import count
-import pyautogui as pg
+import pyautogui as GUI
 import time as t
 import socket as s
 import wmi
@@ -13,7 +13,7 @@ from json import load
 import requests
 import pkg_resources
 
-ctypes.windll.kernel32.SetConsoleTitleW("FxrSys - v2.5")
+ctypes.windll.kernel32.SetConsoleTitleW("FxrSys - v2.6")
 
 
 def main():
@@ -33,7 +33,7 @@ def main():
     2) Display Computer Info
     3) Ascii Art (Text)
     4) IP Tracker
-    5) Counting (Text or Write)
+    5) Webhook Tool
     ''')
 
     option = int(input("Enter the what you want to do (Type the number): "))
@@ -77,9 +77,9 @@ def main():
 
         def spam():
             while t.time() < t_end:
-                pg.typewrite(pastePhrase)
+                GUI.typewrite(pastePhrase)
                 t.sleep(0.1)
-                pg.hotkey("enter")
+                GUI.hotkey("enter")
                 spam()
 
         spam()
@@ -95,62 +95,127 @@ def main():
     def ipTracker():
         YOUR_GEOLOCATION_KEY = 'ec0cb00474be4d55a6bbf6a751609b40'
         ip_address = input('Enter the IP Address you want to track: ')
-        response = requests.get('https://ipgeolocation.abstractapi.com/v1/?api_key=' + YOUR_GEOLOCATION_KEY + '&ip_address=' + ip_address)
+        response = requests.get('https://iGUIeolocation.abstractapi.com/v1/?api_key=' + YOUR_GEOLOCATION_KEY + '&ip_address=' + ip_address)
         result = json.dumps(json.loads(response.content), indent=1)
         print(result)
 
-    def counting():
-        printWrite = input("Print or Write numbers? (P/W): ")
+    # def counting():
+    #     printWrite = input("Print or Write numbers? (P/W): ")
             
-        def print0():
-            endNumber = int(input("Enter the number you want to count upto: "))
-            for i in range(endNumber):
-                print(i)
+    #     def printNum():
+    #         endNumber = int(input("Enter the number you want to count upto: "))
+    #         for i in range(endNumber):
+    #             print(i)
 
 
-        def write():
-                endNumber = int(input("Enter the number you want to count upto: "))
+    #     def writeNum():
+    #             endNumber = int(input("Enter the number you want to count upto: "))
 
-                print(3)
-                t.sleep(1)
-                print(2)
-                t.sleep(1)
-                print(1)
-                t.sleep(1)
-                print("Counting Starting...")
+    #             print(3)
+    #             t.sleep(1)
+    #             print(2)
+    #             t.sleep(1)
+    #             print(1)
+    #             t.sleep(1)
+    #             print("Counting Starting...")
 
-                for i in range(endNumber):
-                    pg.typewrite(i)
+    #             for num in range(1, endNumber + 1):
+    #                 GUI.typewrite(num)
 
-        if printWrite == 'p' or 'P':
-            print0()
-        if printWrite == 'w' or 'W':
-            write()
+    #     if printWrite == 'p' or 'P':
+    #         printNum()
+    #     if printWrite == 'w' or 'W':
+    #         writeNum()
+
+    def webhookTool():
+        print("Made by Pancakes#4891\nand laika#9603")
+        print("\nStay mad if you think stealing this code is wrong.")
+
+        t.sleep(1)
+
+        print('''
+        ╦ ╦┌─┐┌┐ ┬ ┬┌─┐┌─┐┬┌─  ┌┬┐┌─┐┌─┐┬  ┌─┐
+        ║║║├┤ ├┴┐├─┤│ ││ │├┴┐   │ │ ││ ││  └─┐
+        ╚╩╝└─┘└─┘┴ ┴└─┘└─┘┴ ┴   ┴ └─┘└─┘┴─┘└─┘
+        ''')
+
+        def webhook():
+            while True:
+                option = input('''
+                [1] webhook spammer
+                [2] webhook deleter
+                [3] dump webhook info
+                [4] edit webhook
+                [5] exit webhook tools
+                ==> ''')
+                hook = input("webhook: ")
+                
+                if option == "1":
+                    message = input("message to spam: ")
+                    amount = int(input("amount to spam: "))
+                    wait = float(input("delay: "))
+
+                    headers = {"content-type": "application/json"}
+                    data = {"content": message}
+                    t.sleep(wait)
+
+                    for _ in range(amount):
+                        r = requests.post(hook, json=data,headers=headers)
+                        print(f"done sending {amount} messages")
+                        
+                        if r.status_code == 200:
+                            print("sent message successfully")
+
+                if option == "2":
+                    requests.delete(hook)
+                    print("webhook has been deleted!\n") 
+
+                if option == "3":
+                    r = requests.get(hook)
+                    print(r.content)
+                        
+                if option == "4":
+                    wbname = input("name: ")
+                    requests.patch(hook, json={"name": wbname})
+                    print("succsesfully changed webhook name to: ",wbname)
+
+                if option == "5":
+                    if "__main__" == __name__:
+                        main()
+        webhook()
+
 
     if option == int(1):
         Spammer()
         t.sleep(3)
-        main()
+        if "__main__" == __name__:
+            main()
     if option == int(2):
         pcInfo()
         t.sleep(5)
-        main()
+        if "__main__" == __name__:
+            main()
     if option == int(3):
         asciiArt()
         t.sleep(5)
-        main()
+        if "__main__" == __name__:
+            main()
     if option == int(4):
         ipTracker()
         t.sleep(5)
-        main()
+        if "__main__" == __name__:
+            main()
     if option == int(5):
-        counting()
+        webhookTool()
         t.sleep(2)
-        main()
+        if "__main__" == __name__:
+            main()
     if option >= int(6):
         print('Please provide a valid number.')
         t.sleep(2)
-        main()
+        if "__main__" == __name__:
+            main()
 
 
-main()
+if "__main__" == __name__:
+    main()
